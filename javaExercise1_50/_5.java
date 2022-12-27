0ae7ae1a1f6ad986b4db880f0d6f9ede8997322f
john russelle domingo <johnrusselledomingo@gmail.com>
2022-12-27 16:48:13 +0800

    add modification loop

--- javaExercise1_50/_5.java
+++ javaExercise1_50/_5.java
@@ -4,53 +4,61 @@ import java.util.Scanner;
 public class _5 {
   public static void main(String[] args) {
 
-
-//     Input first number: 25
-// Input second number: 5
-// Expected Output :
-// 25 x 5 = 125
+    boolean isOn = true;
     Scanner sc = new Scanner(System.in);
-    System.out.println("Input first number:");
-    int firstVal = sc.nextInt();
-    System.out.println("Input second number:");
-    int secondVal = sc.nextInt();
 
-    System.out.println("Select operation<(+),(-),(*),(/)>:");
-    char operator = sc.next().charAt(0);
-   
-    String op;
-    int ans;
-    switch (operator) {
-      
-      case '+':
-        op = "+";
-        ans = (firstVal + secondVal);
-        break;
+    while (isOn) {
 
-      case '-':
-        op = "-";
-        ans = firstVal - secondVal;
-        break;
+      System.out.println("Greetings");
+      System.out.println("Input first number:");
+      int firstVal = sc.nextInt();
+      System.out.println("Input second number:");
+      int secondVal = sc.nextInt();
+      System.out.println("Select operation<(+),(-),(*),(/)>:");
+      char operator = sc.next().charAt(0);
 
-      case '*':
-        op = "*";
-        ans = firstVal * secondVal;
-        break;
+      String op;
+      int ans;
+
+      switch (operator) {
         
-      case '/':
-        op = "/";
-        ans = firstVal / secondVal;
-        break;  
+        
+        case '+':
+          op = "+";
+          ans = (firstVal + secondVal);
+          break;
 
-      default:
-      op = "?";
-      System.out.println("Unknown operator");
-      ans = 0;
-        break;
+        case '-':
+          op = "-";
+          ans = firstVal - secondVal;
+          break;
+
+        case '*':
+          op = "*";
+          ans = firstVal * secondVal;
+          break;
+          
+        case '/':
+          op = "/";
+          ans = firstVal / secondVal;
+          break;  
+
+        default:
+          op = "?";
+          System.out.println("Unknown operator");
+          ans = 0;
+          isOn = false;
+          break;
+         
+      }
+
+    
+    
+      System.out.println(firstVal+" "+op+" "+secondVal +" = "+ ans);
+      //sc.close();
+      
     }
 
-
-    System.out.println(firstVal+" "+op+" "+secondVal +" = "+ ans);
     sc.close();
     
   }
