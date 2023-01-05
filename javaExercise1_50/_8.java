077e65ad7b2f4259985eea4aa24b4bfd6f475356
john russelle domingo <johnrusselledomingo@gmail.com>
2023-01-05 10:59:05 +0800

    add update

---
+++ javaExercise1_50/_8.java
@@ -0,0 +1,24 @@
+package javaExercise1_50;
+//
+//8. Write a Java program to display the following pattern. Go to the editor
+//        Sample Pattern :
+//
+//        J    a   v     v  a
+//        J   a a   v   v  a a
+//        J  J  aaaaa   V V  aaaaa
+//        JJ  a     a   V  a     a
+//
+
+
+public class _8 {
+    public static void main(String[] args) {
+        System.out.printf("   J   a a    v   v   a a\n");
+        System.out.printf("J  J  Jaaaaa   V V   aaaaa\n");
+        System.out.printf(" JJJ  a     a   V   a     a\n");
+
+    }
+
+
+
+
+}
